1972f275976a83a2a4d6c10714a45c0ab4f7e915
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-05 14:28:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, eventName) {
+    let messages = [];
+    for (let i = 0; i < arrayOfNames.length; i++){
+        let message = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    let i = 0
+     while (i <= number){
+         console.log(number - i);
+         i++;
+     }
+}
